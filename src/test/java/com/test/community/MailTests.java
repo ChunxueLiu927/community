1057c34147930a64e86cb2c6d125e2b777fa023c
ChunxueLiu927 <18340095571@163.com>
2024-02-22 19:50:04 +0800

    2.发送邮件

---
+++ src/test/java/com/test/community/MailTests.java
@@ -0,0 +1,47 @@
+package com.test.community;
+
+import com.test.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * @ClassName MailTests
+ * @Description TODO
+ * @Author lcx
+ * @Date 2024/2/22 19:36
+ * @Version 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1697651929@qq.com", "TEST", "WELCOME.");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1697651929@qq.com", "HTML", content);
+    }
+
+}
