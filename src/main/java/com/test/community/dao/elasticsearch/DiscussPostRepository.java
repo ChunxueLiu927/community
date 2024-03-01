44d021daf588c6fb8dfaaf43898415b49b7a5d76
ChunxueLiu927 <18340095571@163.com>
2024-03-01 16:21:17 +0800

    搜索demo

---
+++ src/main/java/com/test/community/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,9 @@
+package com.test.community.dao.elasticsearch;
+
+import com.test.community.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+}
---
+++ src/test/java/com/test/community/ElasticSearchTest.java
@@ -0,0 +1,75 @@
+package com.test.community;
+
+import com.test.community.dao.DiscussPostMapper;
+import com.test.community.dao.elasticsearch.DiscussPostRepository;
+import com.test.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.text.Highlighter;
+
+/**
+ * @ClassName ElasticSearchTest
+ * @Description TODO
+ * @Author lcx
+ * @Date 2024/2/29 19:58
+ * @Version 1.0
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticSearchTest {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private DiscussPostRepository discussPostRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Test
+    public void testInsert() {
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(241));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(242));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(243));
+    }
+
+    @Test
+    public void testSearchByRepository() {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10)).build();
+//                .withHighlightFields(
+//                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+//                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+//                ).build();
+        Page<DiscussPost> page = discussPostRepository.search(searchQuery);
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getNumber());
+        System.out.println(page.getSize());
+
+    }
+
+}
