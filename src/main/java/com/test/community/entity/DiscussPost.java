72609035e616ff284fd0b6b4a3f675a4a0da935c
ChunxueLiu927 <18340095571@163.com>
2024-03-01 16:20:59 +0800

    搜索demo

--- src/main/java/com/test/community/entity/DiscussPost.java
+++ src/main/java/com/test/community/entity/DiscussPost.java
@@ -1,5 +1,10 @@
 package com.test.community.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
 /**
@@ -9,17 +14,35 @@ import java.util.Date;
  * @Date 2024/2/22 14:24
  * @Version 1.0
  */
+
+@Document(indexName = "discusspost", type = "doc", shards = 6, replicas = 3)
 public class DiscussPost {
 
+    @Id
     private int id;
+
+    @Field(type = FieldType.Integer)
     private int userId;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String title;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;
+
+    @Field(type = FieldType.Integer)
     private int type;
+
+    @Field(type = FieldType.Integer)
     private int status;
 
+    @Field(type = FieldType.Date)
     private Date createTime;
+
+    @Field(type = FieldType.Integer)
     private int commentCount;
+
+    @Field(type = FieldType.Double)
     private double score;
 
     public int getId() {
